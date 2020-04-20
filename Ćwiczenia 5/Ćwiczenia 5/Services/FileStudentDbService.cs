8d43321f2ee0e7577e6edbc8515f13cb1f922d4f
Ryszard <s19234@pjwskt.edu.pl>
2020-04-20 10:19:00 +0200

    Dodano klasę oraz interfejs pod przyszykowaną

---
+++ Ćwiczenia 5/Ćwiczenia 5/Services/FileStudentDbService.cs
@@ -0,0 +1,21 @@
+﻿using Ćwiczenia_5.DTO_s.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ćwiczenia_5.Services
+{
+    public class FileStudentDbService : IStudentDbService
+    {
+        public void EnrollStudent(EnrollStudentRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PromoteStudent(int semester, string studies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ćwiczenia 5/Ćwiczenia 5/Services/IStudentDbService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ćwiczenia_5.DTO_s.Request;
+
+namespace Ćwiczenia_5.Services
+{
+    public interface IStudentDbService
+    {
+        void EnrollStudent(EnrollStudentRequest request);
+        void PromoteStudent(int semester, string studies);
+    }
+}
