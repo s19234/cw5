ed90244da347581dcd42ee8de0636ade4dc0691f
Ryszard <s19234@pjwskt.edu.pl>
2020-04-20 10:47:15 +0200

    Dodano linijkę kodu dla konfiguracji serwisu w metodzie ConfigureService

--- Ćwiczenia 5/Ćwiczenia 5/Startup.cs
+++ Ćwiczenia 5/Ćwiczenia 5/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ćwiczenia_5.Services;
 
 namespace Ćwiczenia_5
 {
@@ -25,6 +26,7 @@ namespace Ćwiczenia_5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IStudentDbService, OracleStudentDbService>();
             services.AddControllers();
         }
 
