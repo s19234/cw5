4d4884ffda5a090e119882553bdc8140174624b3
Ryszard <s19234@pjwskt.edu.pl>
2020-04-20 11:02:49 +0200

    Dodano controller Enrollments

---
+++ Ćwiczenia 5/Ćwiczenia 5/Controllers/EnrollmentsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ćwiczenia_5.DTO_s.Request;
+using Ćwiczenia_5.DTO_s.Responses;
+using Ćwiczenia_5.Models;
+using Ćwiczenia_5.Services;
+
+namespace Ćwiczenia_5.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        private IStudentDbService _service;
+
+        public EnrollmentsController(IStudentDbService service)
+        {
+            this._service = service;
+        }
+
+        [HttpPost]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        {
+            _service.EnrollStudent(request);
+            var response = new EnrollStudentResponse();
+            return Ok(response);
+        }
+    }
+}
