e45b2efe794ef25df662906f509f6e61d3bf1248
Ryszard <s19234@pjwskt.edu.pl>
2020-04-20 10:02:52 +0200

    Initial Commit z dodatkową klasą Student

---
+++ Ćwiczenia 5/Ćwiczenia 5/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ćwiczenia_5.Models
+{
+    public class Student
+    {
+        public string IndexNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Studies { get; set; }
+        public int Semester { get; set; }
+    }
+}
